e1128a5788dc681db42e7ac91acea173a3e14454
EternalHeretic <konakov.egor1995@gmail.com>
2024-09-10 16:14:56 +0700

    Update module_3_2.py
    1

--- lesson3/module_3_2.py
+++ lesson3/module_3_2.py
@@ -26,7 +26,7 @@ def send_email(message:str, recipient:str, * , sender = "university.help@gmail.c
         print(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.")
 
 
-#send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
-#send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender = 'urban.info@gmail.ru')
+send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
+send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender = 'urban.info@gmail.ru')
 send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
-#send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender = 'urban.teacher@mail.ru')
+send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender = 'urban.teacher@mail.ru')
