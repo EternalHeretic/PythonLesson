437120f85bfb4f54b0cb8fa1924848ad46b62df2
EternalHeretic <konakov.egor1995@gmail.com>
2024-09-10 16:14:31 +0700

    Правка
    невнимательность (рукалицо)

--- lesson3/module_3_2.py
+++ lesson3/module_3_2.py
@@ -1,32 +1,32 @@
 
-def send_email(message, recipient, * , sender = "university.help@gmail.com"):
+def send_email(message:str, recipient:str, * , sender = "university.help@gmail.com"):
     pattern = '@'
-    pattern_domen_com = '.com'
-    pattern_domen_ru = '.ru'
-    pattern_domen_net = '.net'
+    pattern_domen_com   = '.com'
+    pattern_domen_ru    = '.ru'
+    pattern_domen_net   = '.net'
 
-    if not all ([
-            pattern in recipient,
-            pattern in sender,
-            recipient.endswith(pattern_domen_ru) or
-            recipient.endswith(pattern_domen_net) or
-            recipient.endswith(pattern_domen_com) or
-            recipient.endswith(pattern_domen_ru) or
-            recipient.endswith(pattern_domen_net) or
-            recipient.endswith(pattern_domen_com)]):
+    if not all ([   pattern in recipient,
+                    pattern in sender,
+                    recipient.endswith(pattern_domen_ru) or
+                    recipient.endswith(pattern_domen_net) or
+                    recipient.endswith(pattern_domen_com),
+                    sender.endswith(pattern_domen_ru) or
+                    sender.endswith(pattern_domen_net) or
+                    sender.endswith(pattern_domen_com)
+                ]):
             print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}.")
-
-    elif sender == "university.help@gmail.com":
-        print(f"Письмо успешно отправлено с адреса {sender} на адрес {recipient}.")
 
     elif recipient == sender:
         print(f"Нельзя отправить письмо самому себе!")
+
+    elif sender == "university.help@gmail.com":
+        print(f"Письмо успешно отправлено с адреса {sender} на адрес {recipient}.")
 
     else:
         print(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.")
 
 
-send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
-send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender = 'urban.info@gmail.ru')
-send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender = 'urban.teacher@mail.uk')
-send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender = 'urban.teacher@mail.ru')
+#send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
+#send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender = 'urban.info@gmail.ru')
+send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
+#send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender = 'urban.teacher@mail.ru')
