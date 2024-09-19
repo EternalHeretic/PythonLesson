00bdec2efd57c302717c820c287f2689fa25757b
EternalHeretic <konakov.egor1995@gmail.com>
2024-09-19 14:37:42 +0700

    update

--- lesson3/module_3_5.py
+++ lesson3/module_3_5.py
@@ -1,11 +1,18 @@
 #Рекурсия
 def get_multiplied_digits(number):
+
     str_number = str(number)
     first = int(str_number[0])
-    if len(str_number) > 1:
-        return first * get_multiplied_digits(int(str(str_number[1:])))
-    else:
-        return int(str_number[-1])
 
-result = get_multiplied_digits(40203)
-print(result)
+    while str_number.endswith('0'):
+            str_number = str_number[:len(str_number)-1]
+    if len(str_number) > 1:
+        return first * get_multiplied_digits(int(str_number[1:]))
+    else:
+        return first
+
+print(get_multiplied_digits(40203))
+print(get_multiplied_digits(240))
+print(get_multiplied_digits(23000))
+print(get_multiplied_digits(42110))
+print(get_multiplied_digits(98001))
