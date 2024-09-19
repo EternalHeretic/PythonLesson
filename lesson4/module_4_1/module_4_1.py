d7b8a9eb6b6736c580e34f9231f0a836946f1608
EternalHeretic <konakov.egor1995@gmail.com>
2024-09-19 15:12:50 +0700

    compleate

---
+++ lesson4/module_4_1/module_4_1.py
@@ -0,0 +1,7 @@
+from fake_math import divide as fk_num
+from true_math import divide as tr_num
+
+print('result1 =', fk_num(69, 3))
+print('result2 =', fk_num(3, 0))
+print('result3 =', tr_num(49, 7))
+print('result4 =', tr_num(15, 0))
