f7a53c6e3a43d63c7b1bc60f874098252dd8c90e
EternalHeretic <konakov.egor1995@gmail.com>
2024-10-07 13:06:34 +0700

    123
    123

--- lesson4/module_4_1/module_4_1.py
+++ lesson4/module_4_1/module_4_1.py
@@ -5,3 +5,11 @@ print('result1 =', fk_num(69, 3))
 print('result2 =', fk_num(3, 0))
 print('result3 =', tr_num(49, 7))
 print('result4 =', tr_num(15, 0))
+
+
+x = 10
+def foo():
+    global x
+    x = 20
+foo()
+print(x)
