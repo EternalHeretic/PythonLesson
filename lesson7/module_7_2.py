568123f57147b4e8e6b51f8cf0259bf619e76ea6
EternalHeretic <konakov.egor1995@gmail.com>
2024-11-09 20:23:56 +0700

    1
    1

--- lesson7/module_7_2.py
+++ lesson7/module_7_2.py
@@ -13,15 +13,14 @@
 def custom_write(file_name, strings):
     strings_positions = {}
     str_num = 0
-    file = open(file_name, 'a', encoding='utf-8')
 
-    for s in strings:
-        str_num += 1
-        start_byte = file.tell()
-        file.write(f'{s}\n')
-        strings_positions[(str_num, start_byte)] = s
+    with open(file_name, 'w', encoding='utf-8') as file:
+        for s in strings:
+            str_num += 1
+            start_byte = file.tell()
+            file.write(f'{s}\n')
+            strings_positions[(str_num, start_byte)] = s
 
-    file.close()
     return strings_positions
 
 info = ['Text for tell.', 'Используйте кодировку utf-8.', 'Because there are 2 languages!', 'Спасибо!']
