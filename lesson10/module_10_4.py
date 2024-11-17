0ff29e16153480701ab9d3694b4b4ea29e948b96
EternalHeretic <konakov.egor1995@gmail.com>
2024-11-17 10:25:51 +0700

    104-5

--- lesson10/module_10_4.py
+++ lesson10/module_10_4.py
@@ -83,3 +83,63 @@
 # Для проверки пустоты очереди используйте метод empty.
 # Для проверки выполнения потока в текущий момент используйте метод is_alive.
 
+from random import randint
+from threading import Thread
+from queue import Queue
+from time import sleep
+
+class Table:
+    def __init__(self, number):
+        self.number = number
+        self.guest = None
+
+class Guest(Thread):
+    def __init__(self, name):
+        super().__init__()
+        self.name = name
+
+    def run(self):
+        sleep(randint(3, 10))
+
+class Cafe:
+    def __init__(self, *tables):
+        self.tables = list(tables)
+        self.queue = Queue()
+
+    def guest_arrival(self, *guests):
+        for guest in guests:
+            guest_seated = False
+            for table in self.tables:
+                if table.guest is None:
+                    table.guest = guest
+                    guest.start()
+                    print(f"{guest.name} сел(-а) за стол номер {table.number}")
+                    guest_seated = True
+                    break
+            if not guest_seated:
+                self.queue.put(guest)
+                print(f"{guest.name} в очереди")
+
+    def discuss_guests(self):
+        while not self.queue.empty() or any(table.guest is not None for table in self.tables):
+            for table in self.tables:
+                if table.guest is not None and not table.guest.is_alive():
+                    print(f"{table.guest.name} покушал(-а) и ушёл(ушла)")
+                    print(f"Стол номер {table.number} свободен")
+                    table.guest = None
+
+                    if not self.queue.empty():
+                        table.guest = self.queue.get()
+                        table.guest.start()
+                        print(f"{table.guest.name} вышел(-ла) из очереди и сел(-а) за стол номер {table.number}")
+
+tables = [Table(number) for number in range(1, 6)]
+
+guests_names = [
+    'Maria', 'Oleg', 'Vakhtang', 'Sergey', 'Darya', 'Arman',
+    'Vitoria', 'Nikita', 'Galina', 'Pavel', 'Ilya', 'Alexandra'
+]
+guests = [Guest(name) for name in guests_names]
+cafe = Cafe(*tables)
+cafe.guest_arrival(*guests)
+cafe.discuss_guests()
--- lesson10/module_10_5.py
+++ lesson10/module_10_5.py
@@ -29,3 +29,29 @@
 # Используйте конструкцию if __name__ == '__main__' при многопроссном подходе.
 # Выводить или возвращать список all_data в функции не нужно. Можете сделать это, но кол-во информации в файлах достигает - 10^9 строк.
 
+import multiprocessing
+from datetime import datetime
+
+def read_info(file_name):
+    all_data = []
+    with open(file_name, 'r') as file:
+        while True:
+            _str = file.readline()
+            if _str == '':
+                break
+            all_data.append(_str)
+
+files = [f'./file {num}.txt' for num in range(1, 5)]
+
+start = datetime.now()
+for file in files:
+    read_info(file)
+end = datetime.now()
+print(end - start)
+
+if __name__ == '__main__':
+    start = datetime.now()
+    with multiprocessing.Pool(processes=4) as pool:
+        pool.map(read_info, files)
+    end = datetime.now()
+    print(end - start)
