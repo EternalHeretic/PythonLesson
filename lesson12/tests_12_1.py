328cbec7bf36bb3fbc1ff683e31a323af44aa4d7
EternalHeretic <konakov.egor1995@gmail.com>
2024-11-26 15:24:52 +0700

    12_1

--- lesson12/tests_12_1.py
+++ lesson12/tests_12_1.py
@@ -25,10 +25,15 @@ class Runner:
     def __init__(self, name):
         self.name = name
         self.distance = 0
-    def walk(self):
-        self.distance += 5
+
     def run(self):
         self.distance += 10
+
+    def walk(self):
+        self.distance += 5
+
+    def __str__(self):
+        return self.name
 
 import unittest
 class RunnerTest(unittest.TestCase):
