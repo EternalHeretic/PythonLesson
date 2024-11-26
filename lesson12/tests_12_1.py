bd23bbcc83dfa82a898741322bd3bbe0c7e426c6
EternalHeretic <konakov.egor1995@gmail.com>
2024-11-26 15:23:53 +0700

    12_1

--- lesson12/tests_12_1.py
+++ lesson12/tests_12_1.py
@@ -20,4 +20,34 @@
 # Ran 3 tests in 0.001s OK
 # Примечания:
 # Попробуйте поменять значения в одном из тестов, результаты
+#
+class Runner:
+    def __init__(self, name):
+        self.name = name
+        self.distance = 0
+    def walk(self):
+        self.distance += 5
+    def run(self):
+        self.distance += 10
 
+import unittest
+class RunnerTest(unittest.TestCase):
+    def test_walk(self):
+        runner = Runner("Алка")
+        for _ in range(10):
+            runner.walk()
+        self.assertEqual(runner.distance, 50)
+    def test_run(self):
+        runner = Runner("Филя")
+        for _ in range(10):
+            runner.run()
+        self.assertEqual(runner.distance, 100)
+    def test_challenge(self):
+        runner1 = Runner("Чебурашка")
+        runner2 = Runner("Шапокляк")
+        for _ in range(10):
+            runner1.run()
+            runner2.walk()
+        self.assertNotEqual(runner1.distance, runner2.distance)
+if __name__ == "__main__":
+    unittest.main()
