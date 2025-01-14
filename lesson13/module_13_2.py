466b8fbbbe1c4b46d2ebe00dcfa77442b7afb406
EternalHeretic <konakov.egor1995@gmail.com>
2025-01-14 09:15:24 +0700

    13_2

--- lesson13/module_13_2.py
+++ lesson13/module_13_2.py
@@ -3,24 +3,27 @@ from aiogram.filters.command import Command
 from aiogram.fsm.storage.memory import MemoryStorage
 import asyncio
 
-API_TOKEN = "7922839201:AAHCwwd1ouZDVkvtI2gQA0MpR9zOpNy2bHg"
+API_TOKEN = "API"
 
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(storage=MemoryStorage())
 
 @dp.message(Command(commands=['start']))
 async def start(message: types.Message):
-    await message.answer("Привет! Я бот, помогающий твоему здоровью.")  # Ответ пользователю
+    # Sending a message to a user but removing the print statement
+    #await message.answer("Привет! Я бот, помогающий твоему здоровью.")
     print("Привет! Я бот, помогающий твоему здоровью.")
 
-@dp.message(Command(commands=['menu']))
-async def start(message: types.Message):
-    await message.answer("Я меню которое ты не увидишь!")  # Ответ пользователю
-    print("Я меню которое ты не увидишь!")
+# @dp.message(Command(commands=['menu']))
+# async def menu(message: types.Message):
+#     # Same as above
+#     #await message.answer("Я меню которое ты не увидишь!")
+#     print("Я меню которое ты не увидишь!")
 
 @dp.message()
 async def all_messages(message: types.Message):
-    await message.answer("Введите команду /start, чтобы начать общение.")  # Ответ пользователю
+    # This message prompts users and is only printed
+    #await message.answer("Введите команду /start, чтобы начать общение.")
     print('Введите команду /start, чтобы начать общение.')
 
 async def main():
